2701835b2ba0638df09ce374ec08116dd9526cbb
Galeilian <wddyhxdyx@126.com>
2022-10-19 14:23:46 -0500

    Done milestone 3, Trying to make the skyline more like a skyline

--- Arc.java
+++ Arc.java
@@ -1,6 +1,8 @@
 import java.awt.*; 
 import javax.swing.*; 
 import java.awt.geom.Path2D;
+import java.applet.*;
+
 
 /**
  * Write a description of class Quadrilateral here.
@@ -8,31 +10,28 @@ import java.awt.geom.Path2D;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Sun
+public class Arc
 {
     // instance variables - replace the example below with your own
     public Color color;
     int centerx;
     int centery;
     int radius;
-    /**
-     * Constructor for objects of class Quadrilateral
-     */
-    public Sun()
-    {
-        // initialise instance variables
-
-    }
+    int angle;
+    int path;
 
     /**
      * Constructor for objects of class Quadrilateral
      */
-    public Sun(int centerx, int centery, int radius, Color color)
+    public Arc(int centerx, int centery, int radius, Color color, int angle, int path)
     {
         this.centerx = centerx;
         this.centery = centery;
         this.radius = radius;
         this.color = color;
+        this.angle = angle;
+        this.path = path;
+        
     }
 
     /**
@@ -41,16 +40,15 @@ public class Sun
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public void draw(Graphics2D g2)
+    public void drawArc(Graphics2D g2)
     {
-        Path2D.Double path = new Path2D.Double();
-
-        path.moveTo(topLeftx, topLefty); // polygon starts at this point
-        path.lineTo(topRightx, topRighty); // next vertex
-        path.lineTo(bottomRightx, bottomRighty); // next vertex
-        path.lineTo(bottomLeftx, bottomLefty); // next vertex
-        path.closePath(); // go back to initial vertex to close the polygon
+        int int1 = centerx - 1/2 * radius;
+        int int2 = centery - 1/2 * radius;
+        int int3 = radius;
+        int int4 = radius;
+        int int5 = angle;
+        int int6 = path;
         g2.setColor(color);
-        g2.fill(path);
+        g2.fillArc(int1,int2,int3,int4,int5,int6); 
     }
 }
--- CityscapeComponent.java
+++ CityscapeComponent.java
@@ -18,8 +18,14 @@ public class CityscapeComponent extends JComponent
         
     Quadrilateral Block1 = new Quadrilateral(70, 70, 140, 70, 140, 140, 70, 140, Color.black); // initialize a new Car object
     Quadrilateral Block2 = new Quadrilateral(35, 35, 70, 35, 60, 70, 35, 70, Color.green); // initialize a new Car object
+    Arc sun = new Arc(100,100,50, Color.orange, 0 ,360);
+    Sky sky = new Sky(Color.blue);
+    
+    
+    
+    Quadrilateral sketch1 = new Quadrilateral(100, 0, 101, 0, 101, 500, 100, 500, Color.black); // initialize a new Car object
 
-     
+
     
     
     // define the CityscapeComponent contructor and intiailize all instance variables
@@ -40,8 +46,11 @@ public class CityscapeComponent extends JComponent
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
+        sky.drawSky(g2);
         Block1.draw(g2);   
         Block2.draw(g2);  
+        sun.drawArc(g2);
+        sketch1.draw(g2);
         
     }
     
